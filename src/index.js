db06063832b3176751c59de87cee1db70213bade
Dean Randel <dee.randel@gmail.com>
2022-10-13 15:35:40 -0600

    Done for today

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import NavBar from './components/navbar'
+import './styles/style.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import CData from './data/crafting.json'
+
+const craftingData = JSON.parse(JSON.stringify(CData));
+console.log(craftingData)
+
+const App = () => {
+    return(
+        <>
+            <NavBar/>
+            <div className="container">
+                <div className="row">
+                    <div className='col-md-4'>Item</div>
+                    <div className='col-md-4'>Details</div>
+                    <div className='col-md-4'>Recpe</div>
+                </div>
+            </div>
+        </>
+    )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App/>); 
